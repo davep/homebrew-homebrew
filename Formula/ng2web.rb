021d71940869fc50234f3d09487e894369dc0fb4
Dave Pearson <davep@davep.org>
2025-03-24 13:54:47 +0000

    :zap: Update ng2web

--- Formula/ng2web.rb
+++ Formula/ng2web.rb
@@ -2,30 +2,30 @@ class Ng2web < Formula
   include Language::Python::Virtualenv
 
   desc "Command line tool for converting Norton Guide Database files to HTML"
-  homepage "https://github.com/davep/ng2web"
-  url "https://files.pythonhosted.org/packages/78/5b/5b0229a24d08859adeccdd698b39051e21b50bc1d0fd9793162425e68d34/ng2web-0.1.1.tar.gz"
-  sha256 "c70f29704736eb6c60c1fa8ac007d2c33b85c187da76411481811d42259d3999"
+  homepage "None"
+  url "https://files.pythonhosted.org/packages/be/2b/3f55899391197ffd211162e750062e9628ce4fab01a44a18db055d89bbda/ng2web-1.0.0.tar.gz"
+  sha256 "1e68cc125059bcc980b3daa2414ee16bc4b36bd62ff3438345e2fd1c5ec82aa6"
 
   depends_on "python3"
 
   resource "Jinja2" do
-    url "https://files.pythonhosted.org/packages/b2/5e/3a21abf3cd467d7876045335e681d276ac32492febe6d98ad89562d1a7e1/Jinja2-3.1.3.tar.gz"
-    sha256 "ac8bd6544d4bb2c9792bf3a159e80bba8fda7f07e81bc3aed565432d5925ba90"
+    url "https://files.pythonhosted.org/packages/df/bf/f7da0350254c0ed7c72f3e33cef02e048281fec7ecec5f032d4aac52226b/jinja2-3.1.6.tar.gz"
+    sha256 "0137fb05990d35f1275a587e9aee6d56da821fc83491a0fb838183be43f66d6d"
   end
 
   resource "MarkupSafe" do
-    url "https://files.pythonhosted.org/packages/87/5b/aae44c6655f3801e81aa3eef09dbbf012431987ba564d7231722f68df02d/MarkupSafe-2.1.5.tar.gz"
-    sha256 "d283d37a890ba4c1ae73ffadf8046435c76e7bc2247bbb63c00bd1a709c6544b"
+    url "https://files.pythonhosted.org/packages/b2/97/5d42485e71dfc078108a86d6de8fa46db44a1a9295e89c5d6d4a06e23a62/markupsafe-3.0.2.tar.gz"
+    sha256 "ee55d3edf80167e48ea11a923c7386f4669df67d7994554387f84e7d8b0a2bf0"
   end
 
   resource "ngdb" do
-    url "https://files.pythonhosted.org/packages/ae/f8/e6c3bb9ac5b456ef75180e5b504492c6dcb65080c96fb65c36fbdf5ef764/ngdb-0.6.2.tar.gz"
-    sha256 "f1dcfe47be89e511ef355193ed9d71c28fea2399d03ae40955f25698a7e3e183"
+    url "https://files.pythonhosted.org/packages/77/4d/57de832c92f193a0c425fe0c8c58275a8fa364b168ae0913f43b806ec21e/ngdb-0.12.0.tar.gz"
+    sha256 "9c1ef674f3cbd5d645df9b4e4f2a40a5e483128aeb927d80de9aec39bda61e1a"
   end
 
   resource "typing-extensions" do
-    url "https://files.pythonhosted.org/packages/16/3a/0d26ce356c7465a19c9ea8814b960f8a36c3b0d07c323176620b7b483e44/typing_extensions-4.10.0.tar.gz"
-    sha256 "b0abd7c89e8fb96f98db18d86106ff1d90ab692004eb746cf6eda2682f91b3cb"
+    url "https://files.pythonhosted.org/packages/df/db/f35a00659bc03fec321ba8bce9420de607a1d37f8342eee1863174c69557/typing_extensions-4.12.2.tar.gz"
+    sha256 "1a7ead55c7e559dd4dee8856e3a88b41225abfe1ce8df57b7c13915fe121ffb8"
   end
 
   def install
